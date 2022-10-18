bbcf6607e05144bcf96dd3a47d7f07b5229412e9
Tharaneshwari <Tharaneshwari>
2022-10-18 12:49:14 +0530

    Messgage

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,24 @@
+
+//Question:3
+package org.company;
+
+public class CompanyInfo {
+private void companyName() {
+	System.out.println("Greens Technology");
+
+}
+private void companyId() {
+	System.out.println(12344);
+
+}
+private void CompanyAddress() {
+	System.out.println("Chennai");
+
+}
+public static void main(String[] args) {
+	CompanyInfo f=new CompanyInfo();
+	f.companyName();
+	f.companyId();
+	f.CompanyAddress();
+}
+}
